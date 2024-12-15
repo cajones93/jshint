45fff3622ba90433ec722d9749cd7e2051e22dcc
cajones93 <jhd7ipdm@students.codeinstitute.net>
2024-12-15 20:13:57 +0000

    added exception modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+
+
+// parse options into a comma separated string
 function processOptions(form){
 
     let optArray = [];
@@ -38,6 +41,7 @@ async function postForm(e) {
         if (response.ok) {
             console.log(displayErrors(data));
         } else {
+            displayException(data);
             throw new Error(data.error);
         }
 }
@@ -74,6 +78,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else{
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -87,5 +92,19 @@ function displayStatus(data){
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     
+    resultsModal.show();
+}
+
+
+function displayException(data){
+    let heading = `An Exception Occurred`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
     resultsModal.show();
 }
